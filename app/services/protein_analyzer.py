5c416ca999880db9327dd2ce8cc6d2b638b65ac6
koredeycode <koredey4u@gmail.com>
2024-09-26 10:37:29 +0100

    this

--- app/services/protein_analyzer.py
+++ app/services/protein_analyzer.py
@@ -7,6 +7,7 @@ def analyze_protein(sequence):
         'molecular_weight': analysis.molecular_weight(),
         'aromaticity': analysis.aromaticity(),
         'isoelectric_point': analysis.isoelectric_point(),
-        'gravy': analysis.gravy()
+        'gravy': analysis.gravy(),
+        'amino_acids_percent()': {aa: percent * 100 for aa, percent in analysis.amino_acids_percent().items()}
     }
     return properties
